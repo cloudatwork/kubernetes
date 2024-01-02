d2746d5cd348555f11f0bb58add011f6843745b3
cloudatwork <143493780+cloudatwork@users.noreply.github.com>
2024-01-02 10:36:04 +0530

    Create install-kubectl.sh

---
+++ install-kubectl.sh
@@ -0,0 +1,59 @@
+# Update the VMs
+sudo apt update
+sudo apt-upgrade -y
+sudo apt-get remove needrestart -y
+
+# Add entries in hosts file
+sudo nano /etc/hosts
+192.168.1.4 master
+192.168.1.5 node1
+192.168.1.6 node2
+
+# Turn off swap (Most cloud VMs don't have swap turned on by default, but still)
+sudo swapoff -a
+
+# Adjust kernel settings
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Reload system
+sudo sysctl --system
+
+# Install a few utils
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" -y
+
+# Update the system
+sudo apt update
+
+# Install container runtime
+sudo apt install -y containerd.io
+
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+# Install kubernetes
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list -y
+
+sudo apt update
+sudo apt install -y kubectl
+sudo apt-mark hold kubectl
